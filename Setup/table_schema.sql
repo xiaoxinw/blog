6a6c5f7a5863d2f7c290a7bd3b7090d1f8a0a895
xiaoxinw <xiaoxing0514@gmail.com>
2018-01-05 14:26:24 +0800

    Change db script.

--- Setup/table_schema.sql
+++ Setup/table_schema.sql
@@ -1,12 +1,12 @@
 -- CREATE DATABASE blog;
 
-CREATE SCHEMA Blog;
+CREATE SCHEMA "Blog";
 
-CREATE TABLE IF NOT EXISTS Blog.Article (
-    Id VARCHAR(32) PRIMARY KEY,
-    Title VARCHAR(128) NOT NULL,
-    Content TEXT NOT NULL,
-    CreateTime TIMESTAMP DEFAULT now(),
-    UpdateTime TIMESTAMP DEFAULT now(),
-    AuthorId VARCHAR(32) DEFAULT '' NOT NULL
+CREATE TABLE IF NOT EXISTS "Blog"."Article" (
+    "Id" VARCHAR(32) PRIMARY KEY,
+    "Title" VARCHAR(128) NOT NULL,
+    "Content" TEXT NOT NULL,
+    "CreateTime" TIMESTAMP DEFAULT now(),
+    "UpdateTime" TIMESTAMP DEFAULT now(),
+    "AuthorId" VARCHAR(32) DEFAULT '' NOT NULL
 );
